82244af4064a2aa8de38f681935aef7d023e3c38
Tyler Smith <tyler.smith.la@gmail.com>
2024-01-13 10:32:55 -0500

    ci: fix deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,5 +47,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|health).*)'],
 };
